5bbbf40fd6b4b8943e20275fa728b3ffe2f72f41
Bidobelemti <mauriceuve3221@gmail.com>
2026-05-17 13:30:31 -0500

    Add: Function extract_data

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -2,5 +2,28 @@ import argparse
 import logging
 import sys
 from pathlib import Path
-
+from src.etl.extract import extract_data
 import pandas as pd
+
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Pipeline ETL - inventariado"
+    )
+    parser.add_argument("--data",   default="../data/02-silver",    help="Path de archivos")
+    parser.add_argument("--output", default="../data/03-gold",       help="Carpeta de salida")
+
+    args = parser.parse_args()
+
+    try:
+        extract_data(args.data)
+    except (FileExistsError, ValueError) as e:
+        logger.error(str(e))
+        sys.exit(1)
